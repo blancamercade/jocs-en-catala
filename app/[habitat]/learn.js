2b4c77ba71a2653e49b09b4af99bb3ab665137a0
blancamercade <145469413+blancamercade@users.noreply.github.com>
2024-12-27 22:57:41 +0100

    Update learn.js

--- app/[habitat]/learn.js
+++ app/[habitat]/learn.js
@@ -4,26 +4,26 @@ import { useState } from "react";
 import * as Speech from "expo-speech";
 
 const animals = {
-  "Animals de casa": [
+  "Casa": [
     { name: "gat", image: require("../../assets/images/gat.jpg") },
     { name: "gos", image: require("../../assets/images/gos.jpg") },
     { name: "conill", image: require("../../assets/images/conill.jpg") },
     { name: "ocell", image: require("../../assets/images/ocell.jpg") },
   ],
-  "Animals de granja": [
+  "Granja": [
     { name: "vaca", image: require("../../assets/images/vaca.jpg") },
     { name: "gallina", image: require("../../assets/images/gallina.jpg") },
     { name: "porc", image: require("../../assets/images/porc.jpg") },
     { name: "ovella", image: require("../../assets/images/ovella.jpg") },
     { name: "pollet", image: require("../../assets/images/pollet.jpg") },
   ],
-  "Animals de bosc": [
+  "Bosc": [
     { name: "os", image: require("../../assets/images/os.jpg") },
     { name: "ren", image: require("../../assets/images/ren.jpg") },
     { name: "guineu", image: require("../../assets/images/guineu.jpg") },
     { name: "esquirol", image: require("../../assets/images/esquirol.jpg") },
   ],
-  "Animals de jungla": [
+  "Jungla": [
     { name: "elefant", image: require("../../assets/images/elefant.jpg") },
     { name: "tigre", image: require("../../assets/images/tigre.jpg") },
     { name: "tucà", image: require("../../assets/images/tuca.jpg") },
